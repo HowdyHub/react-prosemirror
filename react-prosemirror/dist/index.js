7b982787d87b1bfaa5e52c002c4cc10607bf4f49
PjpG <patryk@doppio.jp>
2018-02-06 13:44:06 +0100

    Create prompt

--- react-prosemirror/dist/index.js
+++ react-prosemirror/dist/index.js
@@ -1046,6 +1046,10 @@ var _MenuBarModule = __webpack_require__(18);
 
 var _MenuBarModule2 = _interopRequireDefault(_MenuBarModule);
 
+var _Prompt = __webpack_require__(20);
+
+var _Prompt2 = _interopRequireDefault(_Prompt);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -1056,7 +1060,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var Button = function Button(state, dispatch) {
+var Button = function Button(state, dispatch, refs) {
   return function (item, key) {
     var _classnames;
 
@@ -1070,6 +1074,11 @@ var Button = function Button(state, dispatch) {
         disabled: item.enable && !item.enable(state),
         onMouseDown: function onMouseDown(e) {
           e.preventDefault();
+
+          if (item.title === 'Insert image') {
+            refs.imagePrompt.show();
+          }
+
           item.run(state, dispatch);
         }
       },
@@ -1090,6 +1099,8 @@ var MenuBar = function (_React$Component) {
   _createClass(MenuBar, [{
     key: 'render',
     value: function render() {
+      var _this2 = this;
+
       var _props = this.props,
           menu = _props.menu,
           children = _props.children,
@@ -1109,14 +1120,10 @@ var MenuBar = function (_React$Component) {
           return _react2.default.createElement(
             'span',
             { key: key, className: _MenuBarModule2.default.group },
-            (0, _map2.default)(item, Button(state, dispatch))
+            (0, _map2.default)(item, Button(state, dispatch, _this2.refs))
           );
         }),
-        _react2.default.createElement(
-          'div',
-          { ref: 'imagePrompt' },
-          'Test'
-        )
+        _react2.default.createElement(_Prompt2.default, { ref: 'imagePrompt' })
       );
     }
   }]);
@@ -1206,6 +1213,75 @@ exports.locals = {
 	"button": "_1D-5A2zE6ulZzCSsQaxnbL",
 	"active": "_2WHfJ-FniXIbA-Y_-IM0Rc"
 };
+
+/***/ }),
+/* 20 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Prompt = function (_React$Component) {
+  _inherits(Prompt, _React$Component);
+
+  function Prompt(props) {
+    _classCallCheck(this, Prompt);
+
+    var _this = _possibleConstructorReturn(this, (Prompt.__proto__ || Object.getPrototypeOf(Prompt)).call(this, props));
+
+    _this.state = {
+      show: false
+    };
+    return _this;
+  }
+
+  _createClass(Prompt, [{
+    key: "show",
+    value: function show() {
+      this.setState({ show: true });
+    }
+  }, {
+    key: "hide",
+    value: function hide() {
+      this.setState({ show: false });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      if (!this.state.show) {
+        return null;
+      }
+
+      return _react2.default.createElement(
+        "div",
+        { id: "prompt" },
+        "Test"
+      );
+    }
+  }]);
+
+  return Prompt;
+}(_react2.default.Component);
+
+exports.default = Prompt;
 
 /***/ })
 /******/ ]);
