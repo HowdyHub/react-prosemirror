8c27c0af0da469315380824759e26378f499a6c8
PjpG <patryk@doppio.jp>
2018-02-06 18:39:13 +0100

    try to fix styles

--- react-prosemirror/dist/index.js
+++ react-prosemirror/dist/index.js
@@ -1040,9 +1040,9 @@ var _map = __webpack_require__(16);
 
 var _map2 = _interopRequireDefault(_map);
 
-var _classnames2 = __webpack_require__(17);
+var _classnames3 = __webpack_require__(17);
 
-var _classnames3 = _interopRequireDefault(_classnames2);
+var _classnames4 = _interopRequireDefault(_classnames3);
 
 var _MenuBarModule = __webpack_require__(18);
 
@@ -1071,7 +1071,7 @@ var Button = function Button(state, dispatch, refs) {
       {
         key: key,
         type: 'button',
-        className: (0, _classnames3.default)((_classnames = {}, _defineProperty(_classnames, _MenuBarModule2.default.button, true), _defineProperty(_classnames, _MenuBarModule2.default.active, item.active && item.active(state)), _classnames)),
+        className: (0, _classnames4.default)((_classnames = {}, _defineProperty(_classnames, _MenuBarModule2.default.button, true), _defineProperty(_classnames, _MenuBarModule2.default.active, item.active && item.active(state)), _classnames)),
         title: item.title,
         disabled: item.enable && !item.enable(state),
         onMouseDown: function onMouseDown(e) {
@@ -1097,12 +1097,14 @@ var MenuBar = function (_React$Component) {
   _createClass(MenuBar, [{
     key: 'renderPrompt',
     value: function renderPrompt(prompt, key) {
+      var _classnames2;
+
       var promptProps = prompt.props || {},
           PromptComponent = prompt.component || _Prompt2.default;
 
       return _react2.default.createElement(
         'div',
-        { key: key, className: _MenuBarModule2.default.prompt },
+        { key: key, className: (0, _classnames4.default)((_classnames2 = {}, _defineProperty(_classnames2, _MenuBarModule2.default.prompt, true), _defineProperty(_classnames2, _MenuBarModule2.default.active, this.refs[prompt.reference].show), _classnames2)) },
         _react2.default.createElement(PromptComponent, _extends({ ref: prompt.reference }, promptProps))
       );
     }
@@ -1215,7 +1217,7 @@ exports = module.exports = __webpack_require__(1)(false);
 
 
 // module
-exports.push([module.i, "._18REez13sPdqyVdjc8KyiG {\n  margin-bottom: 5px;\n  display: flex;\n  align-items: baseline;\n}\n\n._28CIjnZhTNlCiXzjH9_CoM {\n  margin-right: 5px;\n}\n\n._1D-5A2zE6ulZzCSsQaxnbL {\n  background: #fff;\n  border: none;\n  font-size: inherit;\n  cursor: pointer;\n  color: #777;\n  border-radius: 0;\n  padding: 5px 10px;\n}\n\n._1D-5A2zE6ulZzCSsQaxnbL:hover {\n  color: #000;\n  background: #f6f6f6;\n}\n\n._1D-5A2zE6ulZzCSsQaxnbL._2WHfJ-FniXIbA-Y_-IM0Rc {\n  color: #000;\n}\n\n._1D-5A2zE6ulZzCSsQaxnbL:disabled {\n  color: #ccc;\n}\n\n._3Y18xWtmRC6mybk9g2iRZw::after {\n  position: absolute;\n  top: 0; right: 0; bottom: 0; left: 0;\n  background: rgba(238, 238, 238, 0.5);\n  content: '';\n}\n\n._3Y18xWtmRC6mybk9g2iRZw > div {\n  position: absolute;\n  top: 10px; right: 10px; left: 10px;\n  padding: 10px 10px;\n  background: #fff; border: 1px solid #ddd;\n  z-index: 1;\n}\n", ""]);
+exports.push([module.i, "._18REez13sPdqyVdjc8KyiG {\n  margin-bottom: 5px;\n  display: flex;\n  align-items: baseline;\n}\n\n._28CIjnZhTNlCiXzjH9_CoM {\n  margin-right: 5px;\n}\n\n._1D-5A2zE6ulZzCSsQaxnbL {\n  background: #fff;\n  border: none;\n  font-size: inherit;\n  cursor: pointer;\n  color: #777;\n  border-radius: 0;\n  padding: 5px 10px;\n}\n\n._1D-5A2zE6ulZzCSsQaxnbL:hover {\n  color: #000;\n  background: #f6f6f6;\n}\n\n._1D-5A2zE6ulZzCSsQaxnbL._2WHfJ-FniXIbA-Y_-IM0Rc {\n  color: #000;\n}\n\n._1D-5A2zE6ulZzCSsQaxnbL:disabled {\n  color: #ccc;\n}\n\n._3Y18xWtmRC6mybk9g2iRZw > div {\n  position: absolute;\n  top: 10px; right: 10px; left: 10px;\n  padding: 10px 10px;\n  background: #fff; border: 1px solid #ddd;\n  z-index: 1;\n}\n\n._3Y18xWtmRC6mybk9g2iRZw._2WHfJ-FniXIbA-Y_-IM0Rc::before {\n  position: absolute;\n  top: 0; right: 0; bottom: 0; left: 0;\n  background: rgba(238, 238, 238, 0.5);\n  content: '';\n}\n", ""]);
 
 // exports
 exports.locals = {
